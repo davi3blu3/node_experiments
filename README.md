561b87080918f62f86cbbe0bc9ea6dca11825d14
Dave Harned <dharned@gmail.com>
2016-08-12 15:17:49 -0500

    updated node, reworked helloworld

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+Abandoning Node:Up and Running after discovering it badly deprecated. Moving on to nodejs.ord documentation
+
+<!--
 Working through examples from: 
 
 "Node: Up and Running" by Tom Hughes-Croucher and Mike Wilson (O’Reilly). Copyright 2012 Tom Hughes-Croucher and Mike Wilson, 978-1-449-39858-3.
 
 http://chimera.labs.oreilly.com/books/1234000001808/index.html
+-->
--- chapter1/helloworld.js
+++ chapter1/helloworld.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'plain'});
-    res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Darlin\'\n');
+});
+
+server.listen (port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
